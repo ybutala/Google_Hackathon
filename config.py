2f64a9ff00eb0b6b3642756c5a8db1f4d5ac006d
ybutala <149416928+ybutala@users.noreply.github.com>
2025-02-15 13:17:36 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+import json
+
+#with open('/etc/config.json') as config_file:
+   # config = json.load(config_file)
+
+class Config:
+    #API_KEY = config.get('API_KEY')
+    API_KEY = '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'
